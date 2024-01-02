0fe124c3633cc6595e6c41d0b98078ab520473f2
fireon4ik <fireon4ik229@mail.ru>
2024-01-02 21:49:59 +0700

    Adding json parser for avatar + Scene jump

--- Assets/Scripts/UI/AvatarSelector.cs
+++ Assets/Scripts/UI/AvatarSelector.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -8,12 +10,41 @@ public class AvatarSelector : MonoBehaviour
     public GameObject circleImageObject;
     public Sprite[] avatars;
     public GameObject objectToDisable;
+    private string dataPath;
+
+    [System.Serializable]
+    public class UserData
+    {
+        [SerializeField]
+        private int id;
+        [SerializeField]
+        private int avatar;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public int Avatar
+        {
+            get { return avatar; }
+            set { avatar = value; }
+        }
+    }
+
+    public static event Action<int> OnAvatarChanged;
 
     void Start()
     {
+        dataPath = Path.Combine(Application.dataPath, "Resources/userData.json");
+
         dropdown.onValueChanged.AddListener(delegate {
             ChangeAvatar(dropdown.value);
+            SaveAvatarSelection(1, dropdown.value); // нужен способ определения id игрока
         });
+
+        LoadAvatarSelection(1); // нужен способ определения id игрока
     }
 
     void ChangeAvatar(int index)
@@ -44,6 +75,36 @@ public class AvatarSelector : MonoBehaviour
         else
         {
             Debug.LogError("Invalid avatar index: " + index);
+        }
+    }
+
+    void SaveAvatarSelection(int id, int index)
+    {
+        if (File.Exists(dataPath))
+        {
+            string json = File.ReadAllText(dataPath);
+            UserData data = JsonUtility.FromJson<UserData>(json);
+            if (data.ID == id)
+            {
+                data.Avatar = index;
+                json = JsonUtility.ToJson(data);
+                File.WriteAllText(dataPath, json);
+                OnAvatarChanged?.Invoke(index);
+            }
+        }
+    }
+
+    void LoadAvatarSelection(int id)
+    {
+        if (File.Exists(dataPath))
+        {
+            string json = File.ReadAllText(dataPath);
+            UserData data = JsonUtility.FromJson<UserData>(json);
+            if (data.ID == id)
+            {
+                dropdown.value = data.Avatar;
+                ChangeAvatar(data.Avatar);
+            }
         }
     }
 }
---
+++ Assets/Scripts/jsonParser/Avatar/AvatarParser.cs
@@ -0,0 +1,61 @@
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AvatarParser : MonoBehaviour
+{
+    public Image avatarImage; // Ссылка на компонент Image, который отображает аватарку в топ меню
+    public Sprite[] avatars; // Массив всех возможных аватарок
+    private string dataPath;
+
+    [System.Serializable]
+    public class UserData
+    {
+        [SerializeField]
+        private int id;
+        [SerializeField]
+        private int avatar;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public int Avatar
+        {
+            get { return avatar; }
+            set { avatar = value; }
+        }
+    }
+
+    void Start()
+    {
+        dataPath = Path.Combine(Application.dataPath, "Resources/userData.json");
+        LoadAvatar(1); // нужен способ определения id игрока
+        AvatarSelector.OnAvatarChanged += OnAvatarChanged; // Подписываемся на событие
+    }
+
+    void OnDestroy()
+    {
+        AvatarSelector.OnAvatarChanged -= OnAvatarChanged; // Отписываемся от события
+    }
+
+    void OnAvatarChanged(int index)
+    {
+        avatarImage.sprite = avatars[index];
+    }
+    void LoadAvatar(int id)
+    {
+        if (File.Exists(dataPath))
+        {
+            string json = File.ReadAllText(dataPath);
+            UserData data = JsonUtility.FromJson<UserData>(json);
+            if (data.ID == id)
+            {
+                avatarImage.sprite = avatars[data.Avatar];
+            }
+        }
+    }
+}
