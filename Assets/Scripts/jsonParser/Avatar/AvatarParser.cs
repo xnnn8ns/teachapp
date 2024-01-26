9e8fbf7daf931805c7aa839ae45b84f9da94c53e
Alexander Bocharov <fireon4ik229@mail.ru>
2024-01-26 14:56:03 +0700

    Fix change avatar in MapScene

--- Assets/Scripts/jsonParser/Avatar/AvatarParser.cs
+++ Assets/Scripts/jsonParser/Avatar/AvatarParser.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AvatarParser : MonoBehaviour
 {
@@ -13,11 +14,13 @@ public class AvatarParser : MonoBehaviour
     {
         LoadAvatar(UserData.UserAvatarID); // нужен способ определения id игрока
         AvatarSelector.OnAvatarChanged += OnAvatarChanged; // Подписываемся на событие
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
     }
 
     void OnDestroy()
     {
         AvatarSelector.OnAvatarChanged -= OnAvatarChanged; // Отписываемся от события
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
     }
 
     void OnAvatarChanged(int index)
@@ -27,5 +30,14 @@ public class AvatarParser : MonoBehaviour
     void LoadAvatar(int id)
     {
         ComonFunctions.LoadAvatarFromResourceByID(avatarImage, UserData.UserAvatarID);
+    }
+
+    void OnSceneUnloaded(Scene scene)
+    {
+        // Если выгружена любая сцена и остались только MapScene и DontDestroyOnLoad
+        if (SceneManager.sceneCount == 2)
+        {
+            LoadAvatar(UserData.UserAvatarID);
+        }
     }
 }
